deb885d8b57e91ec63520f424307f0aab00415a1
MYO-Electronics <113425760+MYO-Electronics@users.noreply.github.com>
2022-11-08 22:55:16 +0800

    Update main_online.py

--- main_online.py
+++ main_online.py
@@ -109,8 +109,7 @@ if __name__ == '__main__':
     print(domain_list)
     set_random_seed(seed)
 
-    for i in range(1): # len(domain_list)
-        i = 0
+    for i in range(len(domain_list)):
         torch.cuda.empty_cache()
         target_list = [domain_list[i]]
         source_list = [x for x in domain_list if x not in target_list]
